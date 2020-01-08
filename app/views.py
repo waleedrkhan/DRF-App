6cdc61b6a3009f5d80d526fdab1ad554faa251fb
Waleed <waleedrkhan90@gmail.com>
2020-01-08 12:52:32 +0500

    updated code with response

--- app/views.py
+++ app/views.py
@@ -3,19 +3,16 @@ from rest_framework.views import APIView
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.response import Response
-from rest_framework.renderers import TemplateHTMLRenderer
-import json
+from django.http import JsonResponse
 
 
 class ProductList(APIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'product_list.html'
 
     def get(self, request, *args, **kwargs):
-        objs = Product.objects.all()
-        return Response({'products': objs})
+        objs = Product.objects.all().values()
+        return JsonResponse(list(objs), safe=False)
 
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -24,4 +21,5 @@ class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
 
     def get(self, request, *args, **kwargs):
         obj = Product.objects.get(pk=kwargs.get('pk', 1))
-        return Response(json.dumps(obj))
+        out = ProductSerializer(obj)
+        return Response(out.data)
--- project/settings.py
+++ project/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'app.apps.AppConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -114,3 +117,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = (
+    'x-requested-with',
+    'content-type',
+    'accept',
+    'origin',
+    'authorization',
+    'x-csrftoken',
+    'Access-Control-Allow-Origin',
+    'Access-Control-Allow-Credentials',
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Methods')
